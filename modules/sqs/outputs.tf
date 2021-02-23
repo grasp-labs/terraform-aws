21e23a9de4f9ad97337bac6d09805362a6505bc3
Yuan Gu <yuan@grasplabs.no>
2021-02-23 16:16:00 +0100

    add arn output for sqs

--- modules/sqs/outputs.tf
+++ modules/sqs/outputs.tf
@@ -2,3 +2,8 @@ output "sqs_id" {
   description = "The URL for the created Amazon SQS queue"
   value = aws_sqs_queue.this.id
 }
+
+output "sqs_arn" {
+  description = "The URL for the created Amazon SQS queue"
+  value = aws_sqs_queue.this.arn
+}
