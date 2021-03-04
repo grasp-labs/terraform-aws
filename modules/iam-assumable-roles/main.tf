bf6d5c189f27adf185abadd60673a4e3e4c423a1
Yuan Gu <yuan@grasplabs.no>
2021-03-04 21:19:25 +0100

    update iam role

--- modules/iam-assumable-roles/main.tf
+++ modules/iam-assumable-roles/main.tf
@@ -1,28 +1,10 @@
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
+resource "aws_iam_role" "this" {
+  name               = var.role_name
+  assume_role_policy = var.iam_role_policy_document_json
 }
 
-resource "aws_iam_role" "_" {
-  name = "${var.project}_role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-
-  description = "Role to allow task definitions to execute role policies and obtain SSM parameters"
-  tags = {
-    service: var.project
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "admin" {
-  count = length(var.role_policy_arns)
-
-  role       = aws_iam_role._.name
-  policy_arn = element(var.role_policy_arns, count.index)
+resource "aws_iam_role_policy_attachment" "attachment" {
+  count      = length(var.policy_arns)
+  role       = aws_iam_role.this.name
+  policy_arn = var.policy_arns[count.index]
 }
--- modules/iam-assumable-roles/variables.tf
+++ modules/iam-assumable-roles/variables.tf
@@ -1,10 +1,15 @@
-variable "project" {
-  description = "Current project."
-  type        = string
+variable "role_name" {
+  description = "Role name"
 }
 
-variable "role_policy_arns" {
-  description = "Policies will be attached to this role."
-  type        = list(string)
+variable "iam_role_policy_document_json" {
+  type = "string"
+
+  description = "Valid JSON Policy String this can be constructed from terraform data.aws_iam_policy_document."
+}
+
+variable "policy_arns" {
+  type        = list
+  description = "List of policy arns to attach to this role"
   default     = []
 }
