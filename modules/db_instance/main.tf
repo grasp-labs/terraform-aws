1b6fe1fb46df0642ca14cef39648b43f94a49e70
Yuan1979 <57373005+Yuan1979@users.noreply.github.com>
2021-03-06 20:40:43 +0100

    Update main.tf

--- modules/db_instance/main.tf
+++ modules/db_instance/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group_rule" "http" {
   cidr_blocks       = [
     "0.0.0.0/0"]
   to_port           = 0
-  type              = ""
+  type              = "ingress"
   security_group_id = aws_security_group._.id
 }
 
