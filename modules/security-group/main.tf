a1fe76132946010f352178f5243cc86a3700ab22
Yuan Gu <yuan@grasplabs.no>
2021-04-13 13:51:39 +0200

    add egress for security group

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -22,3 +22,12 @@ resource "aws_security_group_rule" "ingress" {
     "0.0.0.0/0"]
   security_group_id = join("", aws_security_group.default.*.id)
 }
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = join("", aws_security_group.default.*.id)
+}
