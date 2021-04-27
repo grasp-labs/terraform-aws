753e71144740513b078b0b7376eabca2bdc517b5
Yuan Gu <yuan@grasplabs.no>
2021-04-27 22:08:08 +0200

    add id for alb output

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -3,12 +3,17 @@ output "alb_name" {
   value       = aws_alb.this.name
 }
 
+output "id" {
+  description = "Id of the ALB"
+  value       = aws_alb.this.id
+}
+
 output "http_target_group_arn" {
   description = "The ARN of the http target group."
-  value = aws_alb_target_group.http[0].arn
+  value       = aws_alb_target_group.http[0].arn
 }
 
 output "https_target_group_arn" {
   description = "The ARN of the https target group."
-  value = aws_alb_target_group.https[0].arn
+  value       = aws_alb_target_group.https[0].arn
 }
