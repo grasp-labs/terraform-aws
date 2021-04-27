e683ca7e2b027e41d9aa16e4b2c90fc25f087014
Yuan Gu <yuan@grasplabs.no>
2021-04-27 21:50:18 +0200

    update output

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,10 +1,14 @@
 output "alb_name" {
   description = "The ARN suffix of the ALB"
-  value       = aws_alb.alb.name
+  value       = aws_alb.this.name
 }
 
 output "http_target_group_arn" {
   description = "The ARN of the http target group."
-  #value = join("", aws_alb_target_group.http.*.arn)
   value = aws_alb_target_group.http[0].arn
+}
+
+output "https_target_group_arn" {
+  description = "The ARN of the https target group."
+  value = aws_alb_target_group.https[0].arn
 }
