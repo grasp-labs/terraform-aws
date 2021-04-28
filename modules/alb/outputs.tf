36bff9bc2211ee05321913d97f3bf72585c207d7
Yuan Gu <yuan@grasplabs.no>
2021-04-28 13:58:59 +0200

    tmp

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -12,8 +12,3 @@ output "http_target_group_arn" {
   description = "The ARN of the http target group."
   value       = aws_alb_target_group.http[0].arn
 }
-
-output "https_target_group_arn" {
-  description = "The ARN of the https target group."
-  value       = aws_alb_target_group.https[0].arn
-}
