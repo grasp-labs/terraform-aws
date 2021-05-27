9f879cf17d7571e8d54675c4712a8d7173a1dd0b
Yuan1979 <57373005+Yuan1979@users.noreply.github.com>
2021-05-27 18:11:25 +0200

    Update outputs.tf

--- modules/global-accelerator/outputs.tf
+++ modules/global-accelerator/outputs.tf
@@ -2,3 +2,8 @@ output "ips" {
   description = "Static ips allocated"
   value = aws_globalaccelerator_accelerator.this.ip_sets
 }
+
+output "id" {
+  description = "id of the global accelerator"
+  value = aws_globalaccelerator_accelerator.this.id
+}
