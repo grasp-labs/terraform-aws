4e6416734de35a9449412160786d8a11e060a772
Yuan Gu <yuan@grasplabs.no>
2025-06-29 07:53:05 +0200

    add codebuild

---
+++ modules/codebuild/main.tf
@@ -0,0 +1,112 @@
+resource "aws_iam_role" "codebuild_role" {
+  name = "${var.name}-codebuild-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy" "codebuild_codestar_policy" {
+  name = "${var.name}-codebuild-codestar-policy"
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "codestar-connections:UseConnection"
+        ],
+        Resource = aws_codestarconnections_connection.github.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "codebuild_logs_policy" {
+  name = "${var.name}-codebuild-logs-policy"
+  role = aws_iam_role.codebuild_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_codestarconnections_connection" "github" {
+  name          = var.connection_name
+  provider_type = "GitHub"
+}
+
+resource "aws_codebuild_project" "this" {
+  name          = var.name
+  description   = var.description
+  service_role  = aws_iam_role.codebuild_role.arn
+  build_timeout = 10
+
+  artifacts {
+    type      = "S3"
+    location  = var.artifact_bucket
+    path      = var.artifact_path
+    packaging = "ZIP"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:7.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = var.source_repo_url
+    git_clone_depth = 1
+    buildspec       = var.buildspec_path
+    auth {
+      type     = "CODECONNECTIONS"
+      resource = aws_codestarconnections_connection.github.arn
+    }
+  }
+
+  source_version = var.source_repo_branch
+}
+
+resource "aws_codebuild_webhook" "this" {
+  project_name = aws_codebuild_project.this.name
+  build_type   = "BUILD"
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PULL_REQUEST_MERGED"
+    }
+    filter {
+      type    = "BASE_REF"
+      pattern = var.source_repo_branch
+    }
+  }
+}
+
---
+++ modules/codebuild/variables.tf
@@ -0,0 +1,49 @@
+variable name  {
+  type        = string
+  description = "Name of the CodeBuild project"
+}
+
+variable "description" {
+  type        = string
+  default     = "CodeBuild project for the ds-client"
+  description = "Description of the CodeBuild project"
+}
+
+variable "source_repo_url" {
+  type        = string
+  description = "URL of the source repository for the CodeBuild project"
+}
+
+variable "source_repo_branch" {
+  type        = string
+  default     = "main"
+  description = "Branch of the source repository for the CodeBuild project"
+}
+
+variable "connection_name" {
+  type        = string
+  description = "Name of the CodeBuild connection to the source repository"
+}
+
+variable "artifact_bucket" {
+  type        = string
+  default     = "ds-client-artifacts"
+  description = "S3 bucket for storing build artifacts"
+}
+
+variable "artifact_path" {
+  type        = string
+  description = "Path within the S3 bucket where build artifacts will be stored"
+}
+
+variable "buildspec_path" {
+  type        = string
+  default     = "buildspec.yml"
+  description = "Path to the buildspec file in the source repository"
+}
+
+variable "environment_variables" {
+  type        = map(string)
+  default     = {}
+  description = "Environment variables to pass to the CodeBuild project"
+}
